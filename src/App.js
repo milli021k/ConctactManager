89dc18c444c0958f34351519d2ebfa4661464cad
Milli K <milli2021k@gmail.com>
2022-12-14 19:56:29 -0600

    created components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Headr from "./Headr";
+import AddContact from "./AddContact";
+import ContactsList from "./ContactsList";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Headr></Headr>
+      <AddContact />
+      <ContactsList />
     </div>
   );
 }
-
-export default App;
---
+++ src/Headr.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Headr() {
+  return <div>Conctact Manager</div>;
+}
